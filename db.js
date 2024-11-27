fccbe38f53490e28d83544702ea793d94ec20701
Sandro Teixeira <sandro.jteixeira39@gmail.com>
2024-11-27 19:32:03 -0300

    resolve dependencias do multer

--- db.js
+++ db.js
@@ -5,16 +5,6 @@ async function connect() {
   const pool = new Pool({
     connectionString: process.env.CONNECTION_STRING,
    
-
-  //   host: 'autorack.proxy.rlwy.net',
-  // port: 35108,                     
-  // user: 'postgres',             
-  // password: 'IJeFtvXTUUKpmaagXxnssMiDZwjmvBCN',          
-  // database: 'VendasWeb',       
-  // ssl: {
-  //   rejectUnauthorized: false      
-
-  // }
   });
 
   const client = await pool.connect();
