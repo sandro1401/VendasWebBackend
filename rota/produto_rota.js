6b4aab7b8e2f2d8dcd103e109a7445758d688222
Sandro Teixeira <sandro.jteixeira39@gmail.com>
2024-11-28 11:28:45 -0300

    correçã upload multerConfig

--- rota/produto_rota.js
+++ rota/produto_rota.js
@@ -2,7 +2,7 @@ const express = require('express')
 const controller = require('../controller/produto_controller')
 //const{ upload, processImages } = require('../multerConfig')
 const router = express.Router()
-const upload = require('../cloudinaryConfig')
+const upload = require('../multerConfig')
 
 // router.post('/', controller.addProduto)
 // router.post('/', upload.array('imagem_url'),processImages, controller.addProduto)
